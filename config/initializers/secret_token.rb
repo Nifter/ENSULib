ea6cd5229efce216b602d9d60652c98bd7e03285
Alex Krassikov <a.krassikov@gmail.com>
2012-08-22 16:25:18 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ENSULib::Application.config.secret_token = 'bc037b00cd960583ba73941b83f74eb0c84832c34307e2bdee44d249bcb0484a132d96da034fea8f80bdf34959666f54b3aed783afcb27fa296b9e8a15728d33'
