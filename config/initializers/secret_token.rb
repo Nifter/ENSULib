b0f0ae492d783d077fc9ea2882414164bc3461f4
Alex Krassikov <a.krassikov@gmail.com>
2012-08-23 10:56:10 -0700

    Changed DB to mysql

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-ENSULib::Application.config.secret_token = 'bc037b00cd960583ba73941b83f74eb0c84832c34307e2bdee44d249bcb0484a132d96da034fea8f80bdf34959666f54b3aed783afcb27fa296b9e8a15728d33'
+ENSULib::Application.config.secret_token = '0ab293ed8ba886b7cfca8832f5e6d73107d4633b462969740addf018fc437a564112f880bc8c255a0291704fbca110f7b46c2779b1e5474eea1a5bc07755eb83'
