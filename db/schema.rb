5ed94e849d317c2d7baefcac7d25ca8f766420f7
Alex Krassikov <a.krassikov@gmail.com>
2012-09-05 14:13:39 -0700

    Created a User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,11 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120821203529) do
+ActiveRecord::Schema.define(:version => 20120905210143) do
 
-  create_table "posts", :force => true do |t|
+  create_table "users", :force => true do |t|
     t.string   "name"
-    t.string   "title"
-    t.text     "content"
+    t.string   "email"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
