6f3685b30d1409c3c2632210f2151cd2feaaeca6
Alex Krassikov <a.krassikov@gmail.com>
2012-09-11 12:11:06 -0400

    Added a working index page

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,6 +1,6 @@
 Factory.define :user do |user|
   user.name                  "FirstName LastName"
-  user.email                 "test@example.com"
+  user.email                 "spectester@example.com"
   user.password              "password"
   user.password_confirmation "password"
 end
